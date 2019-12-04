c047fc3a75f90de1125592e47e48d41e2e95b970
Dale Sande <dale.sande@alaskaair.com>
2019-12-04 11:56:59 -0800

    refactor: remove local build of tokens; update to new Tokens npm

--- scripts/tokenScript.js
+++ scripts/tokenScript.js
@@ -34,9 +34,7 @@ console.log('')
 
 
 // Required dependency
-const tokenConfig = require('style-dictionary').extend('./scripts/tokenConfig.json');
 const componentConfig = require('style-dictionary').extend('./scripts/componentConfig.json');
 
 // Style Dictionary build function
-tokenConfig.buildAllPlatforms();
 componentConfig.buildAllPlatforms();
