93dc4ccb8e51d3cec28831ef46a45dd84ad8dbd2
Dale Sande <dale.sande@alaskaair.com>
2019-12-07 16:55:22 -0800

    feat: add new auro-color-avatar element

---
+++ src/auro-color-avatar.js
@@ -0,0 +1,91 @@
+// Copyright (c) Alaska Air. All right reserved. Licensed under the Apache-2.0 license
+// See LICENSE in the project root for license information.
+
+// ---------------------------------------------------------------------
+
+import { html } from "lit-element";
+import { ifDefined } from 'lit-html/directives/if-defined.js';
+import ComponentBase from './component-base';
+import styleCss from "./style-color-avatar-css.js";
+import iconProperties from '@alaskaairux/orion-icons/dist/tokens/CSSTokenProperties-css.js';
+import locationFilles from '@alaskaairux/orion-icons/dist/icons/interface/location-filled_es6.js';
+
+class AuroColorAvatar extends ComponentBase {
+
+  constructor() {
+    super();
+    this.dom = new DOMParser().parseFromString(locationFilles.svg, 'text/html');
+    this.svg = this.dom.body.firstChild;
+  }
+
+  static get properties() {
+    return {
+      ...super.properties,
+      avatartype:   { type: String },
+      colorname:    { type: String }
+    };
+  }
+
+  render() {
+    return html`
+      ${iconProperties}
+      ${styleCss}
+
+
+      <div class="avatarWrapper">
+        ${!this.avatartype ?
+          html`
+          <div class="avatar avatar--color" style="background-color: ${this.varName(this.colorname, 'css')}"></div>
+          ` :
+          html``}
+
+          ${this.avatartype === 'alert' ?
+          html`
+            <div class="avatar">
+              <div class="alertBox icon" style="border-color: ${this.varName(this.colorname, 'css')}"></div>
+            </div>
+          ` :
+          html``}
+
+          ${this.avatartype === 'ui' ?
+          html`
+            <div class="avatar">
+              <div class="uiBox icon" style="background-color: ${this.varName(this.colorname, 'css')}"></div>
+            </div>
+          ` :
+          html``}
+
+          ${this.avatartype === 'border' ?
+          html`
+            <div class="avatar">
+              <div class="icon" style="background-color: ${this.varName(this.colorname, 'css')}"></div>
+            </div>
+          ` :
+          html``}
+
+        ${this.avatartype === 'font' ?
+          html`
+            <div class="avatar" style="color: ${this.varName(this.colorname, 'css')}">
+              Aa
+            </div>
+          ` :
+          html``}
+
+        ${this.avatartype === 'icon' ?
+          html`
+
+          <div class="avatar" style="color: ${this.varName(this.colorname, 'css')}">
+              ${this.svg}
+            </div>
+          ` :
+          html``}
+      </div>
+      <div class="contentWrapper">
+        <p class="avatarToken">auro-${this.colorname}</p>
+      </div>
+    `;
+  }
+}
+
+// define the name of the custom component
+customElements.define("auro-color-avatar", AuroColorAvatar);
